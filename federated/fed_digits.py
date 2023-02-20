2cd56c6be19954a1b88e87b2e291b2f6224b8ab9
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-02-19 18:36:15 -0800

    four datasets

--- federated/fed_digits.py
+++ federated/fed_digits.py
@@ -200,7 +200,7 @@ if __name__ == '__main__':
     parser.add_argument('--lr', type=float, default=1e-2, help='learning rate')
     parser.add_argument('--batch', type = int, default= 32, help ='batch size')
     parser.add_argument('--iters', type = int, default=100, help = 'iterations for communication')
-    parser.add_argument('--wk_iters', type = int, default=1, help = 'optimization iters in local worker between communication')
+    parser.add_argument('--wk_iters', type = int, default=10, help = 'optimization iters in local worker between communication')
     parser.add_argument('--mode', type = str, default='fedbn', help='fedavg | fedprox | fedbn')
     parser.add_argument('--mu', type=float, default=1e-2, help='The hyper parameter for fedprox')
     parser.add_argument('--save_path', type = str, default='../checkpoint/digits', help='path to save the checkpoint')
@@ -258,6 +258,10 @@ if __name__ == '__main__':
 
     # name of each client dataset
     datasets = ['MNIST', 'SVHN', 'USPS', 'SynthDigits', 'MNIST-M']
+
+    datasets=datasets[1:]
+    train_loaders = train_loaders[1:]
+    test_loaders = test_loaders[1:]
     
     # federated setting
     client_num = len(datasets)-3
